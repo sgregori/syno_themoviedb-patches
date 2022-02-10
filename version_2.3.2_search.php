1a100a2aaa98b3ce667cdde4d4447f0e4ca0da29
SalvaG <sgregori@users.noreply.github.com>
2022-02-10 16:44:25 +0100

    Create version_2.3.2_search.php

---
+++ version_2.3.2_search.php
@@ -0,0 +1,9 @@
+// ADD THIS AT LINE 134 - VideoStation/plugins/syno_themoviedb/search.php
+
+        preg_match('/[0-9][0-9][0-9][0-9][0-9][0-9]/', $input['title'], $output);
+
+        if( strlen($output[0])==6 ){
+
+                $id = $output[0];
+
+        }
